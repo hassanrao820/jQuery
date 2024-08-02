7a4e50313db008c04c89cc146da3d4add957afc7
Hassan Akhtar <143855201+hassanrao820@users.noreply.github.com>
2024-08-02 14:56:57 +0500

    jQuery-Notes
    jQuery complete lec's.

---
+++ 1 - Introduction/script.js
@@ -0,0 +1,16 @@
+//? JQuery Introduction.
+        //JQuery is Js library came in 2006.It was created by John Resig.
+            // It is "write less, do more" Js Library.
+//* JQuery Features 
+    // Following Features of JQuey:-
+    // 1: Selectors: Selects HTML elements by id, class, tag, or attribute.
+    // 2: Animation: Animate a div or other element with JQuery.
+    // 3: CSS Manipulation: 
+    // 4: JQuery Ajax:
+    // 5: Event Method: 
+    // 6: jQuery Traversing: 
+    // 7: Plugins: 
+
+// * jQuery - Set Environment
+    // 1: Download JQuery from official website jquery.com
+    // 2: Directly use CDN i.e Content Delivery Network. (https://code.jquery.com/jquery-3.7.1.min.js) 
